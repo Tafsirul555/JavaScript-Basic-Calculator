9ab66078a67f9be5bc53e2b715cb3c3dbcde89a8
Tafsirul555 <mdsamirulmahin@gmail.com>
2025-08-17 00:16:32 +0600

    msg

---
+++ index.js
@@ -0,0 +1,43 @@
+function addition(a, b) {
+    return a + b;
+}
+
+function subtraction(a, b) {
+    return a - b;
+}
+
+function multiplication(a, b) {
+    return a * b;
+}
+
+function division(a, b) {
+    if (b ===0) {
+        return "Error";
+    }
+    return a / b;
+}
+
+function calculator(operation, firstDigit, secondDigit) {
+    if (typeof firstDigit !== "number" || typeof secondDigit !== "number") {
+        return "Error: Invalid number";
+    }
+
+    if (operation === "add") {
+        return addition(firstDigit, secondDigit);
+    } else if (operation === "subtract") {
+        return subtraction(firstDigit, secondDigit);
+    } else if (operation === "multiply") {
+        return multiplication(firstDigit, secondDigit);
+    } else if (operation === "divide") {
+        return division(firstDigit, secondDigit);
+    } else {
+        return "Error: Invalid operation";
+    }
+}
+
+console.log(calculator("add", 40, 4));
+console.log(calculator("subtract", 70, 8));
+console.log(calculator("multiply", 7, 5));
+console.log(calculator("divide", 90, 12));
+console.log(calculator("divide", 100000, 0));
+console.log(calculator("invalid", 9, 3));
